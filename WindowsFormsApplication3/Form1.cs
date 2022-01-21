d98c9caa0a65ac09c8fe02f1aad4bfbf8d4f2a36
Furkan Gayır <63367906+furkangayir@users.noreply.github.com>
2022-01-21 15:22:37 +0300

    Add files via upload

---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double vize, final, sonuc;
+            vize = Convert.ToInt32(textBox1.Text);
+            final = Convert.ToInt32(textBox2.Text);
+            sonuc = (vize * 0.3) + (final * 0.7);
+            if(sonuc<45)
+            {
+                label3.Text = sonuc+" "+"KALDI".ToString();
+                label3.ForeColor = Color.Red;
+            }
+            else if(sonuc>=45)
+            {
+                label3.Text = sonuc+" "+"GEÇTİ".ToString();
+                label3.ForeColor = Color.Green;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is TextBox)
+                {
+                    TextBox txt = (TextBox)item;
+                    txt.Clear();
+                }
+            }
+        }
+    }
+}
